471fb708dff2ae66e917d4059b548af1ceebc79f
Lawrie Pearce <lawrie.pearce@gmail.com>
2019-10-29 15:53:51 +1100

    some changes

--- action_page.php
+++ action_page.php
@@ -1,16 +1,21 @@
 <?php
 /* Attempt MySQL server connection. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-$link = mysqli_connect("localhost", "root", "", "demo");
+$link = mysqli_connect("localhost", "root", "", "form");
  
 // Check connection
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
+
+$fname=name
+$sname=storeName
+$issue=issue
+
  
 // Attempt insert query execution
-$sql = "INSERT INTO persons (first_name, last_name, email) VALUES
-            ('John', 'Rambo', 'johnrambo@mail.com'),
+$sql = "INSERT INTO forms (name, storeName, issue) VALUES
+            ('$fname', '$storeName', '$issue'),
             ('Clark', 'Kent', 'clarkkent@mail.com'),
             ('John', 'Carter', 'johncarter@mail.com'),
             ('Harry', 'Potter', 'harrypotter@mail.com')";
