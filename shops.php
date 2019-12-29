72d42105590a81faf70b076454d15b9618c9c731
Lawrie Pearce <lawrie.pearce@gmail.com>
2019-12-29 16:07:44 +1100

    Added accessible parking page and more shops

---
+++ shops.php
@@ -0,0 +1,41 @@
+<<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "shops";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT storename, storeType, storeLocation, issues, doorWidth, aisleWidth, counterHeight, assistiveTech, accessibleParkingLocation, Distance2Parking FROM begashops ORDER BY storename";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) 
+    
+    {
+        echo "<br>______________________________";
+        echo "<br> Store Name: ". $row["storename"] . "</br>";
+        echo "<br> Store Type: ". $row["storeType"] . "</br>";
+        echo "<br> Store Location: ". $row["storeLocation"] . "</br>";
+        echo "<br> Issues: ". $row["issues"] . "</br>";
+        echo "<br> Door Width: ". $row["doorWidth"] . "</br>";
+        echo "<br> Aisle Width: ". $row["aisleWidth"] . "</br>";
+        echo "<br> Counter Height: ". $row["counterHeight"] . "</br>";
+        echo "<br> Assistive Technology: ". $row["assistiveTech"] . "</br>";
+        echo "<br> Accessible Parking Location: ". $row["accessibleParkingLocation"] . "</br>";
+        echo "<br> Accessible Parking Distance: ". $row["Distance2Parking"] . "</br>";
+         }
+        
+    }
+ else {
+    echo "0 results";
+}
+
+$conn->close();
+?>
---
+++ shops_input.php
@@ -0,0 +1,39 @@
+<?php
+
+$server = "localhost";
+$user = "root";
+$pass = "";
+$dbname = "shops";
+
+//creating connection for mysqli
+
+$conn = new mysqli($server, $user, $pass, $dbname);
+
+//checking connection
+
+if($conn->connect_error){
+    die("connection failed:" . $conn->connect_error);
+}
+
+$storename = mysqli_real_escape_string($conn, $_POST['storename']);
+$storeType = mysqli_real_escape_string($conn, $_POST['storeType']);
+$storeLocation = mysqli_real_escape_string($conn, $_POST['storeLocation']);
+$issues = mysqli_real_escape_string($conn, $_POST['issues']);
+$doorWidth = mysqli_real_escape_string($conn, $_POST['doorWidth']);
+$aisleWidth = mysqli_real_escape_string($conn, $_POST['aisleWidth']);
+$counterHeight = mysqli_real_escape_string($conn, $_POST['counterHeight']);
+$assistiveTech = mysqli_real_escape_string($conn, $_POST['assistiveTech']);
+$accessibleParkingLocation = mysqli_real_escape_string($conn, $_POST['accessibleParkingLocation']);
+$Distance2Parking = mysqli_real_escape_string($conn, $_POST['Distance2Parking']);
+$sql = "INSERT INTO begashops (storename, storeType, storeLocation, issues, doorWidth, aisleWidth, counterHeight, assistiveTech, accessibleParkingLocation, Distance2Parking) VALUES ('$storename', '$storeType', '$storeLocation', '$issues', '$doorWidth', '$aisleWidth', '$counterHeight', '$assistiveTech', '$accessibleParkingLocation', '$Distance2Parking')";
+
+if($conn->query($sql) === TRUE){
+    echo "record added sucessfully";
+    }
+else{
+    echo"Error" . $sql . "<br/>" . $conn->error;
+}
+
+$conn->close();
+
+?>
